007fd45c92b71fc2c8bb8743689092593824cf07
Vitor Freitas <vitorfs@gmail.com>
2021-07-01 21:17:10 -0300

    Update formatting

--- piggybank/settings.py
+++ piggybank/settings.py
@@ -37,11 +37,9 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-
     "rest_framework",
-    'rest_framework.authtoken',
+    "rest_framework.authtoken",
     "django_filters",
-
     "core",
 ]
 
@@ -132,9 +130,9 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 
 REST_FRAMEWORK = {
-    'DEFAULT_AUTHENTICATION_CLASSES': [
-        'rest_framework.authentication.TokenAuthentication',
+    "DEFAULT_AUTHENTICATION_CLASSES": [
+        "rest_framework.authentication.TokenAuthentication",
     ],
     "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
-    "PAGE_SIZE": 100
+    "PAGE_SIZE": 100,
 }
